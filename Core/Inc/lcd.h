9ac91c4409f7b7054119f663c3367ff5c20fe956
fsR3tR0 <fsR3tR0@github.com>
2020-12-10 18:07:17 -0600

    lcd kezelo

---
+++ Core/Inc/lcd.h
@@ -0,0 +1,16 @@
+/*
+ * lcd.h
+ *
+ *  Created on: Dec 10, 2020
+ *      Author: root
+ */
+
+#ifndef LCD_H_
+#define LCD_H_
+
+#include "main.h"
+
+void LCD_cmd(u8 cmd);
+
+
+#endif /* LCD_H_ */
---
+++ Core/Src/lcd.c
@@ -0,0 +1,8 @@
+/*
+ * lcd.c
+ *
+ *  Created on: Dec 10, 2020
+ *      Author: root
+ */
+#include "lcd.h"
+
